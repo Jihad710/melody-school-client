c761b6324ca66149547fb58b20676a2f65936955
Faruk Rahman Jihad <jihadmahmud71@gmail.com>
2023-06-06 16:54:59 -0600

    navbar design done

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
+    </div>
+    
   </React.StrictMode>,
 )
