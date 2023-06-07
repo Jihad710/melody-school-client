8ad3e54b14da349fcf66008053c95a9044186f3f
Faruk Rahman Jihad <jihadmahmud71@gmail.com>
2023-06-07 03:25:23 -0600

    banner section done

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,54 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../../../assets/Banner/1.jpg';
+import img2 from '../../../assets/Banner/2.avif';
+
+const Banner = () => {
+    return (
+        <Carousel>
+      <div className="carousel-item relative">
+          <img src={img1}/>
+          <div className="absolute h-full flex items-center left-0 bottom-10 ">
+            <div className='text-white space-y-7 pl-20'>
+                <h2 className='text-6xl  font-normallight'>Music For Everyone</h2>
+                <div>
+               
+                <button className="btn btn-outline btn-warning">Start Learning</button>
+                </div>
+            </div>
+          </div>
+          
+        </div> 
+      <div className="carousel-item relative">
+          <img src={img1}/>
+          <div className="absolute h-full flex items-center left-0 bottom-10 ">
+            <div className='text-white space-y-7 pl-20'>
+                <h2 className='text-6xl  font-normallight'>Music For Everyone</h2>
+                <div>
+               
+                <button className="btn btn-outline btn-warning">Start Learning</button>
+                </div>
+            </div>
+          </div>
+          
+        </div> 
+      <div className="carousel-item relative">
+          <img src={img1}/>
+          <div className="absolute h-full flex items-center left-0 bottom-10 ">
+            <div className='text-white space-y-7 pl-20'>
+                <h2 className='text-6xl  font-normallight'>Music For Everyone</h2>
+                <div>
+               
+                <button className="btn btn-outline btn-warning">Start Learning</button>
+                </div>
+            </div>
+          </div>
+          
+        </div> 
+      
+            
+        </Carousel>
+    );
+};
+
+export default Banner;
